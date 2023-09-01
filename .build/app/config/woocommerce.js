dc52f629eff4262947dfba183018e089634a6a62
khushi220201 <121846843+khushi220201@users.noreply.github.com>
2023-09-01 20:10:29 +0530

    added in database

--- .build/app/config/woocommerce.js
+++ .build/app/config/woocommerce.js
@@ -1,7 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.API = void 0;
-var woocommerce_rest_api_1 = require("@woocommerce/woocommerce-rest-api");
+const woocommerce_rest_api_1 = __importDefault(require("@woocommerce/woocommerce-rest-api"));
 //import WooCommerceAPI from "@woocommerce/woocommerce-rest-api";
 //  WOOCOMMERCE CONFIGRATION
 exports.API = new woocommerce_rest_api_1.default({
@@ -10,4 +13,3 @@ exports.API = new woocommerce_rest_api_1.default({
     consumerSecret: process.env.CONSUMER_SECRET,
     version: "wc/v3", // API version (optional, defaults to 'wc/v3')
 });
-//# sourceMappingURL=woocommerce.js.map
--- app/handler.ts
+++ app/handler.ts
@@ -1,9 +1,33 @@
+import { prisma } from "./config/db";
 import { API } from "./config/woocommerce";
 
 exports.handler = async (event: any) => {
   try {
-    const response = await API.get("customers")
+    const response = await API.get("customers");
     console.log("Customers data:", response.data);
+    const createdCustomers = [];
+    for (const customerData of response.data) {
+      const createdCustomer = await prisma.customer.create({
+        data: {
+          id: customerData.id,
+          date_created: new Date(customerData.date_created),
+          date_created_gmt: new Date(customerData.date_created_gmt),
+          date_modified: new Date(customerData.date_modified),
+          date_modified_gmt: new Date(customerData.date_modified_gmt),
+          email: customerData.email,
+          first_name: customerData.first_name,
+          last_name: customerData.last_name,
+          role: customerData.role,
+          username: customerData.username,
+          is_paying_customer: customerData.is_paying_customer,
+          avatar_url: customerData.avatar_url,
+          meta_data: customerData.meta_data,
+        },
+      });
+      createdCustomers.push(createdCustomer);
+    }
+
+    console.log("Created Customers:", createdCustomers);
 
     return {
       statusCode: 200,
